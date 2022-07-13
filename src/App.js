21c2d1a4896310399024b81fb22fe55054d8b3e2
Filip Spiridonov <fspiridonov@uber.com>
2022-07-13 16:25:50 -0400

    Add an example to demonstrate touch issue on mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const RESET_TIME_MS = 300;
+
+const App = () => {
+  const [events, setEvents] = useState([]);
+  const [clickLocation, setClickLocation] = useState(null);
+
+  let lastEventTimestamp = Date.now();
+  const handler = (event) => {
+    const now = Date.now()
+    if (now - lastEventTimestamp > RESET_TIME_MS) {
+      setEvents([event.type]);
+    } else {
+      setEvents([...events, event.type]);
+    }
+    lastEventTimestamp = now;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        position: "fixed",
+        left: 0,
+        top: 0,
+        height: "100vh",
+        width: "100vw",
+      }}
+      onClick={(event) => {
+        setClickLocation({ x: event.clientX, y: event.clientY });
+      }}
+    >
+      {clickLocation && (
+        <svg
+          style={{
+            position: "fixed",
+            left: 0,
+            top: 0,
+            height: "100%",
+            width: "100%",
+            pointerEvents: "none",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <circle cx={clickLocation.x} cy={clickLocation.y} r="10" />
+        </svg>
+      )}
+      <div
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "lightgrey",
+          border: "1px solid black",
+          margin: 50,
+        }}
+        onTouchStart={handler}
+        onTouchEnd={handler}
+        onPointerDown={handler}
+        onPointerUp={handler}
+        onMouseDown={handler}
+        onMouseUp={handler}
+        onClick={handler}
+      >
+        {events.map((event, index) => (
+          <div key={`${event}-${index}`}>{event}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
